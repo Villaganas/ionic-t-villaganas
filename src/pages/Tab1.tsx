ab7b4d93ef92fd570ee4213815f6cd4f56249007
Bimbo E. Villaganas <20201259@nbsc.edu.ph>
2024-03-23 13:39:00 +0800

    Added Home page

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar, IonItem, IonLabel, IonIcon, IonAlert, IonButton,IonActionSheet } from '@ionic/react';
-import React, { useState } from 'react';
+import React from 'react';
+import { IonImg, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar, IonItem, IonLabel, IonIcon, IonList, IonListHeader, IonFooter, IonInput, IonButton } from '@ionic/react';
 import { logoFacebook, logoTwitter, logoInstagram } from 'ionicons/icons';
 import './Tab1.css';
-
 
 const Tab1: React.FC = () => {
   return (
@@ -14,11 +13,11 @@ const Tab1: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <div className="cover-photo">
-          <img src="https://scontent.fcgy2-1.fna.fbcdn.net/v/t39.30808-6/354231992_1319969795582819_162204273908998251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFWNKDVFubVCMm5e17mLRxdMkcZmb3xK7oyRxmZvfErus_cglVOjz9XERNnqW81D0WsDVInJRMCJF8zo0I4lowh&_nc_ohc=56NxX-JMhx0AX_GHXly&_nc_ht=scontent.fcgy2-1.fna&oh=00_AfA4ZV6cK5rRT5deCaiSIfnToIF1PBntiU9ADCZU_SUuYA&oe=65F05344" alt="Cover" />
+          <img src="https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/354231992_1319969795582819_162204273908998251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFWNKDVFubVCMm5e17mLRxdMkcZmb3xK7oyRxmZvfErus_cglVOjz9XERNnqW81D0WsDVInJRMCJF8zo0I4lowh&_nc_ohc=lwqg-_ki8EoAX-NafJ5&_nc_zt=23&_nc_ht=scontent.fdvo2-2.fna&oh=00_AfCpZMSBqY7uErArXeVsczCWBtg2e-Jlgp0DyVHQL9VhUw&oe=66021F84" alt="Cover" />
         </div>
         <div className="profile-info">
           <IonAvatar className="avatar">
-            <img src="https://scontent.fmnl9-2.fna.fbcdn.net/v/t39.30808-6/423583714_1444891803090617_4793189191939464381_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGl5A39vM92hA5ErEMAjCv2ZXDTm0Na27JlcNObQ1rbsp5Ul0wAFj9_JbdxCDOsPtvuoatpXT6aHADqwV134QR8&_nc_ohc=jZ5jgO7K2HsAX-2pfLu&_nc_ht=scontent.fmnl9-2.fna&oh=00_AfAsQldN8byicbbocPggK2Y4xRnSI4meMxDnGhRO7fTwAg&oe=65F5320C" alt="Profile" />
+            <img src="hhttps://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-1/423583714_1444891803090617_4793189191939464381_n.jpg?stp=dst-jpg_p200x200&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGl5A39vM92hA5ErEMAjCv2ZXDTm0Na27JlcNObQ1rbsp5Ul0wAFj9_JbdxCDOsPtvuoatpXT6aHADqwV134QR8&_nc_ohc=mc0792_2MLEAX9rCELC&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfBHZ0QRAYTyqG5RYlKge-JH4E5b_rNpR1iGGdf0p86fTg&oe=66020B0A" alt="Profile" />
           </IonAvatar>
           <div className="profile-details">
             <h1>Bimbo E. Villaganas</h1>
@@ -30,101 +29,89 @@ const Tab1: React.FC = () => {
 
         {/* Additional Content */}
         <div className="additional-content">
-  <div>
-    <h2>About Me</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget sem nec nulla venenatis eleifend.</p>
-  </div>
+          <IonList>
+            <IonListHeader>
+              <h2>About Me</h2>
+            </IonListHeader>
+            <IonItem>
+              <IonLabel>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget sem nec nulla venenatis eleifend.</IonLabel>
+            </IonItem>
+          </IonList>
 
-  <div>
-    <h2>Interests</h2>
-    <ul className="interests-list">
-      <li>Traveling</li>
-      <li>Photography</li>
-      <li>Reading</li>
-      <li>Music</li>
-      <li>Art</li>
-    </ul>
-  </div>
+          <IonList>
+            <IonListHeader>
+              <h2>Interests</h2>
+            </IonListHeader>
+            <IonItem>
+              <IonLabel>Traveling</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Photography</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Reading</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Music</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Art</IonLabel>
+            </IonItem>
+          </IonList>
 
-  <div>
-    <h2>Skills</h2>
-    <ul className="skills-list">
-      <li>Web Development</li>
-      <li>Graphic Design</li>
-      <li>Content Writing</li>
-      <li>Project Management</li>
-      <li>Communication</li>
-    </ul>
-  </div>
-</div>
+          <IonList>
+            <IonListHeader>
+              <h2>Skills</h2>
+            </IonListHeader>
+            <IonItem>
+              <IonLabel>Web Development</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Graphic Design</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Content Writing</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Project Management</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Communication</IonLabel>
+            </IonItem>
+          </IonList>
+        </div>
 
-<IonButton id="present-alert"expand="block">Read more</IonButton>
-      <IonAlert
-        trigger="present-alert"
-        header="A Short Title Is Best"
-        subHeader="A Sub Header Is Optional"
-        message="A message should be a short, complete sentence."
-        buttons={['Action']}
-      ></IonAlert>
-      <IonButton id="open-action-sheet" expand="block">More option</IonButton>
-      <IonActionSheet
-        trigger="open-action-sheet"
-        header="Actions"
-        buttons={[
-          {
-            text: 'Delete',
-            role: 'destructive',
-            data: {
-              action: 'delete',
-            },
-          },
-          {
-            text: 'Share',
-            data: {
-              action: 'share',
-            },
-          },
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            data: {
-              action: 'cancel',
-            },
-          },
-        ]}
-      ></IonActionSheet>
-      
-      
-<footer>
-  <div className="footer-content">
-    <div className="footer-section">
-      <h3>Contact Us</h3>
-      <p>Email: 20201259@nbsc.edu.ph</p>
-      <p>Phone: 09123456789</p>
-      <p>Address: Upper Pol-Oton, Tankulan, Manolo Fortich, Bukidnon</p>
-    </div>
-    <div className="footer-section">
-  <h3>Follow Us</h3>
-  <ul className="social-links">
-    <li><a href="#"><IonIcon icon={logoFacebook} /></a></li>
-    <li><a href="#"><IonIcon icon={logoTwitter} /></a></li>
-    <li><a href="#"><IonIcon icon={logoInstagram} /></a></li>
-  </ul>
-</div>
-
-    <div className="footer-section">
-      <h3>Newsletter</h3>
-      <p>Subscribe to our newsletter for updates.</p>
-      <form>
-        <input type="email" placeholder="Your email" />
-        <button type="submit">Subscribe</button>
-      </form>
-    </div>
-  </div>
-  <div className="footer-bottom">
-    <p>&copy; 2024 Bimbo E. Villaganas. All rights reserved.</p>
-  </div>
-</footer>
+        <IonFooter>
+          <div className="footer-content">
+            <div className="footer-section">
+              <h3>Contact Us</h3>
+              <p>Email: 20201259@nbsc.edu.ph</p>
+              <p>Phone: 09123456789</p>
+              <p>Address: Upper Pol-Oton, Tankulan, Manolo Fortich, Bukidnon</p>
+            </div>
+            <div className="footer-section">
+              <h3>Follow Us</h3>
+              <IonList className="social-links">
+                <IonItem><a href="#"><IonIcon icon={logoFacebook} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoTwitter} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoInstagram} />BIMBO VILLAGANAS</a></IonItem>
+              </IonList>
+            </div>
+            <div className="footer-section">
+              <h3>Newsletter</h3>
+              <p>Subscribe to our newsletter for updates.</p>
+              <form>
+                <IonItem>
+                  <IonInput type="email" placeholder="Your email" name="email" />
+                  <IonButton type="submit">Subscribe</IonButton>
+                </IonItem>
+              </form>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <p>&copy; 2024 Bimbo E. Villaganas. All rights reserved.</p>
+          </div>
+        </IonFooter>
 
       </IonContent>
     </IonPage>
