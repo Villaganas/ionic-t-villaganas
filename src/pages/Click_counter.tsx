8b80c9734855937470b1435b1c183b1c677b3da5
Bimbo Villaganas <20201259@nbsc.edu.ph>
2024-04-11 23:47:08 +0800

    added the footer

--- src/pages/Click_counter.tsx
+++ src/pages/Click_counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
+import { IonFooter, IonList, IonInput, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
 import { logoFacebook, logoTwitter, logoInstagram } from 'ionicons/icons';
 import './Click_couter.css';
 
@@ -26,6 +26,38 @@ const Tab2: React.FC = () => {
           <p>Total Clicks: {count}</p>
           <IonButton onClick={incrementCounter}>Click me!</IonButton>
         </div>
+        <IonFooter>
+          <div className="footer-content">
+            <div className="footer-section">
+              <h3>Contact Us</h3>
+              <p>Email: 20201259@nbsc.edu.ph</p>
+              <p>Phone: 09123456789</p>
+              <p>Address: Upper Pol-Oton, Tankulan, Manolo Fortich, Bukidnon</p>
+            </div>
+            <div className="footer-section">
+              <h3>Follow Us</h3>
+              <IonList className="social-links">
+                <IonItem><a href="#"><IonIcon icon={logoFacebook} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoTwitter} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoInstagram} />BIMBO VILLAGANAS</a></IonItem>
+              </IonList>
+            </div>
+            <div className="footer-section">
+              <h3>Newsletter</h3>
+              <p>Subscribe to our newsletter for updates.</p>
+              <form>
+                <IonItem>
+                  <IonInput type="email" placeholder="Your email" name="email" />
+                  <IonButton type="submit">Subscribe</IonButton>
+                </IonItem>
+              </form>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <p>&copy; 2024 Bimbo E. Villaganas. All rights reserved.</p>
+          </div>
+        </IonFooter>
+
 
       </IonContent>
     </IonPage>
