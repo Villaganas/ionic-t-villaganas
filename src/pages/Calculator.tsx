454ff1ca0e0a677838a48e70492541899edd1ea6
Bimbo Villaganas <20201259@nbsc.edu.ph>
2024-04-11 10:45:55 +0800

    Added Calculator

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -25,7 +25,47 @@ const Tab3: React.FC = () => {
 
   return (
     <IonPage>
-    
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className="calculator-container">
+          <div className="result">{result ? result : expression}</div>
+          <div className="buttons">
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('7')}>7</IonButton>
+              <IonButton onClick={() => handleButtonClick('8')}>8</IonButton>
+              <IonButton onClick={() => handleButtonClick('9')}>9</IonButton>
+              <IonButton onClick={() => handleButtonClick('/')}>/</IonButton>
+            </div>
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('4')}>4</IonButton>
+              <IonButton onClick={() => handleButtonClick('5')}>5</IonButton>
+              <IonButton onClick={() => handleButtonClick('6')}>6</IonButton>
+              <IonButton onClick={() => handleButtonClick('*')}>*</IonButton>
+            </div>
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('1')}>1</IonButton>
+              <IonButton onClick={() => handleButtonClick('2')}>2</IonButton>
+              <IonButton onClick={() => handleButtonClick('3')}>3</IonButton>
+              <IonButton onClick={() => handleButtonClick('-')}>-</IonButton>
+            </div>
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('0')}>0</IonButton>
+              <IonButton onClick={() => handleButtonClick('.')}>.</IonButton>
+              <IonButton onClick={() => handleButtonClick('C')} id="c-button">C</IonButton>
+              <IonButton onClick={() => handleButtonClick('+')}>+</IonButton>
+            </div>
+            <div className="row">
+            <IonButton onClick={() => handleButtonClick('=')} id="equals-button">=</IonButton>
+            </div>
+          </div>
+        </div>
+
+
+      </IonContent>
     </IonPage>
   );
 };
