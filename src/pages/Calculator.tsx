12ef58244480a956f85c36c08dc455889dd377b8
Bimbo Villaganas <20201259@nbsc.edu.ph>
2024-04-11 11:02:44 +0800

    change the footer

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
-import { logoFacebook, logoTwitter, logoInstagram } from 'ionicons/icons';
+import { IonFooter, IonList, IonInput, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
+import { logoFacebook, logoTwitter, logoInstagram} from 'ionicons/icons';
 import './Calculator.css';
 
 const Tab3: React.FC = () => {
@@ -63,37 +63,37 @@ const Tab3: React.FC = () => {
             </div>
           </div>
         </div>
-        <footer>
-  <div className="footer-content">
-    <div className="footer-section">
-      <h3>Contact Us</h3>
-      <p>Email: 20201259@nbsc.edu.ph</p>
-      <p>Phone: 09123456789</p>
-      <p>Address: Upper Pol-Oton, Tankulan, Manolo Fortich, Bukidnon</p>
-    </div>
-    <div className="footer-section">
-  <h3>Follow Us</h3>
-  <ul className="social-links">
-    <li><a href="#"><IonIcon icon={logoFacebook} /></a></li>
-    <li><a href="#"><IonIcon icon={logoTwitter} /></a></li>
-    <li><a href="#"><IonIcon icon={logoInstagram} /></a></li>
-  </ul>
-</div>
-
-
-    <div className="footer-section">
-      <h3>Newsletter</h3>
-      <p>Subscribe to our newsletter for updates.</p>
-      <form>
-        <input type="email" placeholder="Your email" />
-        <button type="submit">Subscribe</button>
-      </form>
-    </div>
-  </div>
-  <div className="footer-bottom">
-    <p>&copy; 2024 Bimbo E. Villaganas. All rights reserved.</p>
-  </div>
-</footer>
+        <IonFooter>
+          <div className="footer-content">
+            <div className="footer-section">
+              <h3>Contact Us</h3>
+              <p>Email: 20201259@nbsc.edu.ph</p>
+              <p>Phone: 09123456789</p>
+              <p>Address: Upper Pol-Oton, Tankulan, Manolo Fortich, Bukidnon</p>
+            </div>
+            <div className="footer-section">
+              <h3>Follow Us</h3>
+              <IonList className="social-links">
+                <IonItem><a href="#"><IonIcon icon={logoFacebook} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoTwitter} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoInstagram} />BIMBO VILLAGANAS</a></IonItem>
+              </IonList>
+            </div>
+            <div className="footer-section">
+              <h3>Newsletter</h3>
+              <p>Subscribe to our newsletter for updates.</p>
+              <form>
+                <IonItem>
+                  <IonInput type="email" placeholder="Your email" name="email" />
+                  <IonButton type="submit">Subscribe</IonButton>
+                </IonItem>
+              </form>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <p>&copy; 2024 Bimbo E. Villaganas. All rights reserved.</p>
+          </div>
+        </IonFooter>
 
       </IonContent>
     </IonPage>
