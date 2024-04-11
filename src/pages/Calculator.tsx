60ff6232d198890f1a2d9cadf3f413cd1b69e39f
Bimbo Villaganas <20201259@nbsc.edu.ph>
2024-04-11 11:05:33 +0800

    change the footer

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
-import { logoFacebook, logoTwitter, logoInstagram } from 'ionicons/icons';
+import { IonFooter, IonList, IonInput, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAvatar, IonItem, IonLabel, IonIcon } from '@ionic/react';
+import { logoFacebook, logoTwitter, logoInstagram} from 'ionicons/icons';
 import './Calculator.css';
 
 const Tab3: React.FC = () => {
@@ -25,7 +25,77 @@ const Tab3: React.FC = () => {
 
   return (
     <IonPage>
-    
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className="calculator-container">
+          <div className="result">{result ? result : expression}</div>
+          <div className="buttons">
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('9')}>9</IonButton>
+              <IonButton onClick={() => handleButtonClick('8')}>8</IonButton>
+              <IonButton onClick={() => handleButtonClick('7')}>7</IonButton>
+              <IonButton onClick={() => handleButtonClick('/')}>/</IonButton>
+            </div>
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('6')}>6</IonButton>
+              <IonButton onClick={() => handleButtonClick('5')}>5</IonButton>
+              <IonButton onClick={() => handleButtonClick('4')}>4</IonButton>
+              <IonButton onClick={() => handleButtonClick('*')}>*</IonButton>
+            </div>
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('3')}>3</IonButton>
+              <IonButton onClick={() => handleButtonClick('2')}>2</IonButton>
+              <IonButton onClick={() => handleButtonClick('1')}>1</IonButton>
+              <IonButton onClick={() => handleButtonClick('-')}>-</IonButton>
+            </div>
+            <div className="row">
+              <IonButton onClick={() => handleButtonClick('0')}>0</IonButton>
+              <IonButton onClick={() => handleButtonClick('.')}>.</IonButton>
+              <IonButton onClick={() => handleButtonClick('C')} id="c-button">C</IonButton>
+              <IonButton onClick={() => handleButtonClick('+')}>+</IonButton>
+            </div>
+            <div className="row">
+            <IonButton onClick={() => handleButtonClick('=')} id="equals-button">=</IonButton>
+            </div>
+          </div>
+        </div>
+        <IonFooter>
+          <div className="footer-content">
+            <div className="footer-section">
+              <h3>Contact Us</h3>
+              <p>Email: 20201259@nbsc.edu.ph</p>
+              <p>Phone: 09123456789</p>
+              <p>Address: Upper Pol-Oton, Tankulan, Manolo Fortich, Bukidnon</p>
+            </div>
+            <div className="footer-section">
+              <h3>Follow Us</h3>
+              <IonList className="social-links">
+                <IonItem><a href="#"><IonIcon icon={logoFacebook} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoTwitter} />BIMBO VILLAGANAS</a></IonItem>
+                <IonItem><a href="#"><IonIcon icon={logoInstagram} />BIMBO VILLAGANAS</a></IonItem>
+              </IonList>
+            </div>
+            <div className="footer-section">
+              <h3>Newsletter</h3>
+              <p>Subscribe to our newsletter for updates.</p>
+              <form>
+                <IonItem>
+                  <IonInput type="email" placeholder="Your email" name="email" />
+                  <IonButton type="submit">Subscribe</IonButton>
+                </IonItem>
+              </form>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <p>&copy; 2024 Bimbo E. Villaganas. All rights reserved.</p>
+          </div>
+        </IonFooter>
+
+      </IonContent>
     </IonPage>
   );
 };
